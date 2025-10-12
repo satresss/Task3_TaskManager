cc39a5248954204ca313af81269d1dc06cc96929
Artem Savchenko <sarts5840@gmail.com>
2025-10-12 22:31:52 +0300

    ver 0.5 Added Files No code ver 0.5

---
+++ Task3_TaskManager/Tasks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_TaskManager
+{
+    public class Tasks
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
